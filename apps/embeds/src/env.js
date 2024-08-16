39ee44d8eabf64d3d369d259f2ef0e54095e03d3
Arth <probablyarth@gmail.com>
2024-08-16 23:12:33 +0530

    redirect to gg from embeds

--- apps/embeds/src/env.js
+++ apps/embeds/src/env.js
@@ -19,6 +19,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_MAIN_APP_URL: z.string(),
   },
 
   /**
@@ -27,6 +28,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_MAIN_APP_URL: process.env.NEXT_PUBLIC_MAIN_APP_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
